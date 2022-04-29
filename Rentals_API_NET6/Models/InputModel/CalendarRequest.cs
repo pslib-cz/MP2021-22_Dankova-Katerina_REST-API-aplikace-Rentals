0a82f429af60edc39d6acb438e41b51a0aece259
Kateřina Daňková <katerina.dankova@pslib.cz>
2022-04-29 20:19:05 +0200

    clear

--- Rentals_API_NET6/Models/InputModel/CalendarRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rentals_API_NET6.Models.InputModel
-{
-    public class CalendarRequest
-    {
-        public int Month { get; set; }
-        public int Year { get; set; }
-        public List<int> Items { get; set; }
-    }
-}
